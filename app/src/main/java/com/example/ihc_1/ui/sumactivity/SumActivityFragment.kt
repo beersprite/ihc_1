ea77534ae6bda61d2272de57127a2d3d6d496184
yasmin beer <55171112+beersprite@users.noreply.github.com>
2023-06-11 23:30:27 -0300

    task 1

--- app/src/main/java/com/example/ihc_1/ui/sumactivity/SumActivityFragment.kt
+++ app/src/main/java/com/example/ihc_1/ui/sumactivity/SumActivityFragment.kt
@@ -1,10 +1,10 @@
 package com.example.ihc_1.ui.sumactivity
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -19,7 +19,6 @@ class SumActivityFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    private lateinit var sumButton: Button
     private lateinit var sumResult: Number
 
     override fun onCreateView(
@@ -42,28 +41,19 @@ class SumActivityFragment : Fragment() {
             resultSumText.text = it
         }
 
-        sumButton = requireActivity().findViewById<View>(com.example.ihc_1.R.id.sum_button) as Button
-
-        return root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        sumButton.setOnClickListener{
-
+        binding.sumButton.setOnClickListener{
             val firstNumber = binding.editTextNumberSigned.text.toString()
-            print(firstNumber)
             val secondNumber = binding.editTextNumberSigned2.text.toString()
-            print(secondNumber)
 
             if (firstNumber != "" && secondNumber != "" ){
-                sumResult = firstNumber.toFloat() + secondNumber.toFloat()
+                sumResult = firstNumber.toInt() + secondNumber.toInt()
+                resultSumText.text = "Result is $sumResult"
 
             }
         }
 
+        return root
     }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
--- app/src/main/java/com/example/ihc_1/ui/sumactivity/SumActivityViewModel.kt
+++ app/src/main/java/com/example/ihc_1/ui/sumactivity/SumActivityViewModel.kt
@@ -3,7 +3,6 @@ package com.example.ihc_1.ui.sumactivity
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.ihc_1.R
 
 class SumActivityViewModel : ViewModel() {
 
@@ -13,6 +12,7 @@ class SumActivityViewModel : ViewModel() {
     private val _result_sum_text = MutableLiveData<String>().apply {
         value = "Result is "
     }
+
     val description_text: LiveData<String> = _description_text
     val result_sum_text: LiveData<String> = _result_sum_text
 }
