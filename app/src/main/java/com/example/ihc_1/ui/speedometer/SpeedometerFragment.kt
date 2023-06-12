08acc9d19ecada394c86d58ef903db2f7ef84f00
yasmin beer <55171112+beersprite@users.noreply.github.com>
2023-06-12 13:17:37 -0300

    task 1.3 - speedometer rotation sensor working

--- app/src/main/java/com/example/ihc_1/ui/speedometer/SpeedometerFragment.kt
+++ app/src/main/java/com/example/ihc_1/ui/speedometer/SpeedometerFragment.kt
@@ -1,28 +1,25 @@
 package com.example.ihc_1.ui.speedometer
 
 import android.content.Context
-import android.content.Context.SENSOR_SERVICE
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat.getSystemService
-import com.example.ihc_1.R
-import com.example.ihc_1.databinding.FragmentCopyActivityBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.ihc_1.databinding.FragmentSpeedometerBinding
-import com.example.ihc_1.databinding.FragmentSumActivityBinding
-import com.example.ihc_1.ui.copyactivity.CopyFragmentViewModel
 
 class SpeedometerFragment : Fragment(), SensorEventListener {
 
     private var _binding: FragmentSpeedometerBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var sensorManager: SensorManager
+    private lateinit var sensorAccelerometer: Sensor
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,33 +27,49 @@ class SpeedometerFragment : Fragment(), SensorEventListener {
         savedInstanceState: Bundle?
     ): View {
         val speedometerViewModel =
-            ViewModelProvider(this).get(SpeedometerViewModel::class.java)
+            ViewModelProvider(this)[SpeedometerViewModel::class.java]
 
         _binding = FragmentSpeedometerBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        var mSensorManager = SENSOR_SERVICE as SensorManager
-        var mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        sensorManager = activity?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
 
         return root
     }
 
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this, sensorAccelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+    }
 
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
     override fun onSensorChanged(event: SensorEvent?) {
+
         if( event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
             val sensorX = event.values[0]
             val sensorY = event.values[1]
             val sensorZ = event.values[2]
 
-            binding.xAxis.setText(sensorX.toString())
-            binding.yAxis.setText(sensorY.toString())
-            binding.zAxis.setText(sensorZ.toString())
+            binding.xAxis.setText("X  $sensorX")
+            binding.yAxis.setText("Y  $sensorY")
+            binding.zAxis.setText("Z  $sensorZ")
 
         }
     }
 
-    override fun onAccuracyChanged(event: Sensor?, accuracy: Int) {
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
         //TODO("Not yet implemented")
+    }
+
+    override fun onDestroyView() {
+        sensorManager.unregisterListener(this)
+
+        super.onDestroyView()
+        _binding = null
     }
 
 }
